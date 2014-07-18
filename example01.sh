5be04b5e6241e7a280b15396ce7bbf2815ab9184
Proteu5 <MichaelCohee@Live.Com>
2014-07-17 21:30:02 -0400

    Created Example *.Sh Miner Script File
    This is what your *.sh files should look like; or something similar. You do not need any 'sleep' or 'echo' commands. It think it just looks cool. However, I do like to throw in a 'sleep 1' at the beginning just in case ;)

---
+++ example01.sh
@@ -0,0 +1,11 @@
+echo "Priming Device..."
+sleep 1
+echo "Loading ZeusMiner-Blizzard"
+sleep 1
+echo "Device Found"
+echo "Preparing LTC Mining Configuration"
+sleep 1
+echo "Please StandBy, Proteu5..."
+cd
+cd /root/zeus-bfg/bfgminer
+sudo ./bfgminer -c /root/zeus-bfg/bfgminer/zus1.conf

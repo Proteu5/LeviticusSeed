9379d9d84281d2965e13729083bfe87e9895d259
Proteu5 <MichaelCohee@Live.Com>
2014-07-17 21:30:53 -0400

    Update example01.sh

--- example01.sh
+++ example01.sh
@@ -6,6 +6,9 @@ echo "Device Found"
 echo "Preparing LTC Mining Configuration"
 sleep 1
 echo "Please StandBy, Proteu5..."
+sleep 5
+echo "I hope you set my premissions: chmod +x example01.sh"
+sleep 1
 cd
 cd /root/zeus-bfg/bfgminer
 sudo ./bfgminer -c /root/zeus-bfg/bfgminer/zus1.conf
